a92f1143224c1712855dfb33f2ab429466c3cf49
Angrymouse <rykov.nikas@gmail.com>
2022-09-13 23:38:44 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# HNSD.js
+
+Want to use handshake protocol in your app, but don't want user to install any software besides your? Now you can builtin HNSD binaries with JS interface to your project easily!
+As easy as that:
+```js
+import HNSDResolver from "./index.js";
+let resolver = new HNSDResolver();
+await resolver.launch();
+resolver.height // Sync height of your HNSD
+resolver.recursivePort // Recursive port of your HNSD node (host is always 127.0.0.1)
+resolver.authoritativePort // Root-only port of your HNSD node (host is always 127.0.0.1)
+resolver.rootResolver // Already configured to HNSD's authoritative port BNS's Resolver instance
+resolver.recursiveResolver // Already configured to HNSD's recursive port BNS's Resolver instance
+```
---
+++ index.js
@@ -0,0 +1,68 @@
+import * as os from "os";
+import * as path from "path";
+import getPort from "get-port";
+import { dns } from "bns";
+import * as cp from "child_process";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+let executable = (
+	{
+		win32: () => {
+			return path.join(__dirname, "binaries", "win32", "hnsd.exe");
+		},
+		linux: () => {
+			return path.join(__dirname, "binaries", "linux", "hnsd");
+		},
+		darwin: () => {
+			return path.join(__dirname, "binaries", "darwin", "MacOS", "hnsd");
+		},
+	}[os.platform()] ||
+	(() => {
+		throw new Error("Unsupported platform");
+	})
+)();
+export default class HNSDResolver {
+	constructor() {
+		this.height = 0;
+		this.recursiveResolver = new dns.Resolver({
+			tcp: true,
+			inet6: true,
+			edns: true,
+			dnssec: true,
+		});
+		this.rootResolver = new dns.Resolver({
+			tcp: true,
+			inet6: true,
+			edns: true,
+			dnssec: true,
+		});
+	}
+	async launch() {
+		let classThis = this;
+		let [recursivePort, authoritativePort] = [await getPort(), await getPort()];
+		this.recursivePort = recursivePort;
+		this.authoritativePort = authoritativePort;
+		this.recursiveResolver.setServers(["127.0.0.1:" + recursivePort]);
+		this.rootResolver.setServers(["127.0.0.1:" + authoritativePort]);
+		let hnsd = cp.spawn(executable, [
+			`--ns-host`,
+			`127.0.0.1:${authoritativePort}`,
+			`--rs-host`,
+			`127.0.0.1:${recursivePort}`,
+		]);
+		hnsd.stdout.on("data", (data) => {
+			let newHeightRegEx = /^chain \((\d+)\)/gm;
+			let capturer = newHeightRegEx.exec(data.toString());
+			if (!capturer || !capturer[1]) {
+				return;
+			}
+			let capturedHeight = parseInt(capturer[1]);
+			if (capturedHeight > classThis.height) {
+				classThis.height = capturedHeight;
+			}
+		});
+		hnsd.on("exit", (code) => {
+			throw new Error("HNSD exited, code " + code);
+		});
+	}
+}
