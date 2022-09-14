44fc758f44c2ae9bb129f765442c634fcb9ed4b6
Angrymouse <rykov.nikas@gmail.com>
2022-09-14 21:09:21 +0300

    add new block event, launch resolves only after full sync

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import * as path from "path";
 import getPort from "get-port";
 import { dns } from "bns";
 import * as cp from "child_process";
+import EventEmitter from "events";
 import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 let executable = (
@@ -21,9 +22,12 @@ let executable = (
 		throw new Error("Unsupported platform");
 	})
 )();
-export default class HNSDResolver {
+export default class HNSDResolver extends EventEmitter {
 	constructor() {
+		super();
 		this.height = 0;
+		this.lastCheckHeight = 0;
+		this.synced = false;
 		this.recursiveResolver = new dns.Resolver({
 			tcp: true,
 			inet6: true,
@@ -37,32 +41,48 @@ export default class HNSDResolver {
 			dnssec: true,
 		});
 	}
-	async launch() {
+	launch() {
 		let classThis = this;
-		let [recursivePort, authoritativePort] = [await getPort(), await getPort()];
-		this.recursivePort = recursivePort;
-		this.authoritativePort = authoritativePort;
-		this.recursiveResolver.setServers(["127.0.0.1:" + recursivePort]);
-		this.rootResolver.setServers(["127.0.0.1:" + authoritativePort]);
-		let hnsd = cp.spawn(executable, [
-			`--ns-host`,
-			`127.0.0.1:${authoritativePort}`,
-			`--rs-host`,
-			`127.0.0.1:${recursivePort}`,
-		]);
-		hnsd.stdout.on("data", (data) => {
-			let newHeightRegEx = /^chain \((\d+)\)/gm;
-			let capturer = newHeightRegEx.exec(data.toString());
-			if (!capturer || !capturer[1]) {
-				return;
-			}
-			let capturedHeight = parseInt(capturer[1]);
-			if (capturedHeight > classThis.height) {
-				classThis.height = capturedHeight;
-			}
-		});
-		hnsd.on("exit", (code) => {
-			throw new Error("HNSD exited, code " + code);
+		return new Promise(async (resolve, reject) => {
+			let [recursivePort, authoritativePort] = [
+				await getPort(),
+				await getPort(),
+			];
+			this.recursivePort = recursivePort;
+			this.authoritativePort = authoritativePort;
+			this.recursiveResolver.setServers(["127.0.0.1:" + recursivePort]);
+			this.rootResolver.setServers(["127.0.0.1:" + authoritativePort]);
+			let hnsd = cp.spawn(executable, [
+				`--ns-host`,
+				`127.0.0.1:${authoritativePort}`,
+				`--rs-host`,
+				`127.0.0.1:${recursivePort}`,
+			]);
+			hnsd.stdout.on("data", (data) => {
+				let newHeightRegEx = /^chain \((\d+)\)/gm;
+				let capturer = newHeightRegEx.exec(data.toString());
+				if (!capturer || !capturer[1]) {
+					return;
+				}
+				let capturedHeight = parseInt(capturer[1]);
+				if (capturedHeight > classThis.height) {
+					classThis.height = capturedHeight;
+					classThis.emit("newHeight", capturedHeight);
+				}
+			});
+
+			let synccheck = setInterval(() => {
+				if (classThis.height - classThis.lastCheckHeight < 5) {
+					classThis.synced = true;
+					classThis.emit("finishedSync");
+					clearInterval(synccheck);
+					resolve();
+				}
+				classThis.lastCheckHeight = classThis.height;
+			}, 20000);
+			hnsd.on("exit", (code) => {
+				throw new Error("HNSD exited, code " + code);
+			});
 		});
 	}
 }
